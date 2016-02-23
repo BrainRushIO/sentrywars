48e8917ea0b124790c70de7787fbc7865a504148
paleonluna <paleonluna@gmail.com>
2016-02-22 18:32:46 -0800

    scene and more

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -12,16 +12,16 @@ public class InputController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
 		RaycastHit hit;
 		if (Physics.Raycast (ray, out hit, raycastDistance)) {
 			if (hit.collider.tag == "Floor") {
 				targetBubble.transform.position = hit.point;
-				targetBubble.SetActive (true);
+//				targetBubble.SetActive (true);
 
 			} else {
-				targetBubble.SetActive (false);
+//				targetBubble.SetActive (false);
 
 			}
 
