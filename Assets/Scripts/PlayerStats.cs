0a7c59c3449b87e6e22dae9ab679098f71be6467
paleonluna <paleonluna@gmail.com>
2016-03-10 22:30:16 -0800

    progress on UI and such

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,18 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class PlayerStats : MonoBehaviour {
 
-	public float currentEnergy;
-	public float energyPerSecond;
+public class PlayerStats : NetworkBehaviour {
+	[SyncVar]
+	float currentEnergy = 50f;
+	[SyncVar]
+	float energyUptake = 1f;
+	[SyncVar]
+	float energyTimer = 0;
+
+	public float GetCurrentEnergy() {
+		return currentEnergy;
+	}
+
+	public bool TryToSpendEnergy (float howMuchEnergy) {
+		if (howMuchEnergy <= currentEnergy) {
+			currentEnergy -= howMuchEnergy;
+			return true;
+		} else {
+			return false;
+		}
+	}
+		
+	[SerializeField] Text currentEnergyText, energyUptakeText;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		currentEnergyText.text = "Energy: " + currentEnergy.ToString ("F0");
+		energyUptakeText.text = "Energy Uptake: " + energyUptake + "/sec";
+
+		HandleTimer ();
+	}
+
+	void HandleTimer() {
+		if (energyTimer >= 1f) {
+			currentEnergy += energyUptake;
+			energyTimer = 0;
+		} 
+		currentEnergy += Time.deltaTime;
 	}
 }
