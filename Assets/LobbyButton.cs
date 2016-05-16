8db78943c01b246ca798bbe87b70ee5a0c12820c
Pablo <pablo@brainrush.io>
2016-05-16 15:19:40 -0700

    adding in Lobby

---
+++ Assets/LobbyButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class LobbyButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartLobby() {
+		NetworkLobbyManager.singleton.StartHost ();
+
+	}
+
+	public void JoinLobby () {
+		
+		NetworkLobbyManager.singleton.StartClient ();
+	}
+}
