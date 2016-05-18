85d7bf6fccfecf0daa47c12c900d665e4963d523
Pablo <pablo@brainrush.io>
2016-05-18 13:30:31 -0700

    added in pull face and username data

---
+++ Assets/ProfileCanvas.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProfileCanvas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public Text usernameText;
+	public Image profileImage;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/UserSelectionInterface.cs
+++ Assets/UserSelectionInterface.cs
@@ -1,24 +1,66 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using SimpleJson;
 
 public class UserSelectionInterface : MonoBehaviour {
 
 	//TODO make this automated
-
+	string[] imageURLs, usernames;
+	List<Texture> profileImages;
 	float verticalAngle = 15f;
 	float horizontalAngle = 15f;
+	[SerializeField] Rester thisRester;
+	string profileInfoListURL = "sw-tournament.herokuapp.com/gamers";
 
 	public List<GameObject> userProfilePivots;
 	// Use this for initialization
 	void Start () {
-		for(int i = 0; i < transform.childCount ; i++) {
+		for(int i = 0; i < transform.childCount; i++) {
 			userProfilePivots.Add (transform.GetChild(i).gameObject);
 		}
+		thisRester = GameObject.FindObjectOfType <Rester> ();
+		GetProfileInfoList ();
 	}
-	
+		
+
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void GetProfileInfoList() {
+		thisRester.GetJSON (profileInfoListURL, ( err, result) => {
+			InterpretJSON(result);// = result;
+		});
+	}
+
+	void InterpretJSON (JsonObject thisJSON) {
+		object tempUsernamesObj, tempImageURLsObj;
+		thisJSON.TryGetValue ("playerUsernames", out tempUsernamesObj);
+		thisJSON.TryGetValue ("playerProfileURLs", out tempImageURLsObj);
+		usernames = tempUsernamesObj.ToString ().Split (',');
+		imageURLs = tempImageURLsObj.ToString ().Split (',');
+		for (int i = 0; i < usernames.Length; i++) {
+			userProfilePivots [i].transform.GetChild (0).GetComponent<ProfileCanvas> ().usernameText.text = usernames [i];
+			StartCoroutine(SetProfileImage (i, imageURLs [i]));
+		}
+	}
+
+
+	IEnumerator SetProfileImage(int index, string url) {
+		// Start a download of the given URL
+		WWW www = new WWW(url);
+
+		// Wait for download to complete
+		yield return www;
+
+		// assign texture
+		Texture2D temp = www.texture;
+//		Image thisProfileImage = userProfilePivots [index].transform.GetChild (0).GetComponent<ProfileCanvas> ().profileImage;
+		Sprite thisSprite = Sprite.Create (temp, new Rect(0,0, temp.width, temp.height), new Vector2(.5f,.5f));
+
+		userProfilePivots [index].transform.GetChild (0).GetComponent<ProfileCanvas> ().profileImage.sprite = thisSprite;
 	}
 }
