4c41280eeb9f57c94d50efceaebde701fdc8cc51
Pablo <pablo@brainrush.io>
2016-05-27 10:51:59 -0700

    adding in SelectableUI to game

--- Assets/Scripts/VRUI.cs
+++ Assets/Scripts/VRUI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class VRUI : MonoBehaviour {
 
 	public GameObject currentlyHighlightedObject;
-
+	public GameObject VRUIPanel;
 	// Use this for initialization
 	void Start () {
 	
@@ -18,11 +18,29 @@ public class VRUI : MonoBehaviour {
 	void FixedUpdate () {
 
 		RaycastHit hit;
-		if (Physics.Raycast(transform.position,transform.forward, out hit, 1000f)) {
+		if (Physics.Raycast (transform.position, transform.forward, out hit, 1000f)) {
 			if (hit.transform.tag == "VRUIObject") {
 				hit.transform.GetComponent<VRUIObject> ().HoverOver ();
 				currentlyHighlightedObject = hit.transform.gameObject;                                                                                                                                                                                                                                                                                                                                                                                       
 			}
+		} else {
+			currentlyHighlightedObject = null;
 		}
+	}
+
+	void SelectVRUI() {
+		if (currentlyHighlightedObject != null) {
+			VRUIPanel.SetActive (false);
+			//select building type
+			//enter buildmode
+		}
+	}
+
+	void OnEnable() {
+		InputController.OnRightTriggerFingerDown += SelectVRUI;
+	}
+
+	void OnDisable() {
+		InputController.OnRightTriggerFingerDown -= SelectVRUI;
 	}
 }
